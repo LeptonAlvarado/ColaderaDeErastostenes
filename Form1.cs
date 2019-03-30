d6abaebf2b498d70afde3063de1f06d974f3545d
LeptonAlvarado <48077048+LeptonAlvarado@users.noreply.github.com>
2019-03-29 22:05:58 -0600

    Se subio la clase como el Form
    Clase y codigo el form para la implementacion del codigo

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColaderaDeEratostenes
+{
+    public partial class Form1 : Form
+    {
+        ColaderaVector vec = new ColaderaVector();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            txtMostrar.Text = vec.Mostrar();
+        }
+
+        private void btnColadera_Click(object sender, EventArgs e)
+        {
+            vec.IniUno();
+            vec.Coladera();
+        }
+    }
+}
